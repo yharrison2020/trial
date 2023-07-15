54da969103fae7a99326000c0d1694665d25bd5c
yharrison2020 <yharrison@gmail.com>
2023-07-15 20:25:10 +0100

    first dev commit

--- test.R
+++ test.R
@@ -2,3 +2,8 @@ library(readxl)
 
 metrics <- read_excel("input/Extraction.xlsx", sheet="Extraction")
 View(metrics)
+
+for (row in 1:nrow(metrics)) {
+  metric_code <- metrics[row, "Metric"]
+  print(paste("the metric is ", metric_code))
+}
